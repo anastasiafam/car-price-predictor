a3ca2b849946e9acacf456470d1e09eae6b33b23
Katyzina <katyzinan@gmail.com>
2025-06-07 13:10:09 +0300

    Добавила несколько тестов

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,9 @@
 # tests/test_api.py
-import requests
+from fastapi.testclient import TestClient
+from app.api.main import app
 
-BASE_URL = "http://localhost:8000/predict"
+client = TestClient(app)
 
-# Валидный пример
 valid_sample = {
     "Manufacturer": "HONDA",
     "Model": "CIVIC",
@@ -23,48 +23,48 @@ valid_sample = {
     "Levy_rate": 0.05,
 }
 
-#Тест на успешное предсказание
+# Тест на успешное предсказание
 def test_api_prediction():
-    response = requests.post(BASE_URL, json=valid_sample)
+    response = client.post("/predict", json=valid_sample)
     assert response.status_code == 200
     json_data = response.json()
     assert "predicted_price" in json_data
     assert isinstance(json_data["predicted_price"], (float, int))
 
-#Тест на обязательные поля
+# Тест на обязательные поля
 def test_api_missing_field():
     sample = valid_sample.copy()
     del sample["Model"]  # Удаляем обязательное поле
-    response = requests.post(BASE_URL, json=sample)
+    response = client.post("/predict", json=sample)
     assert response.status_code == 422
     assert "detail" in response.json()
 
-#Тест на типы данных
+# Тест на типы данных
 def test_api_invalid_field_type():
     sample = valid_sample.copy()
     sample["Mileage"] = "много"  # Строка вместо числа
-    response = requests.post(BASE_URL, json=sample)
+    response = client.post("/predict", json=sample)
     assert response.status_code == 422
     assert "detail" in response.json()
 
-#Тест на пустой запрос
+# Тест на пустой запрос
 def test_api_empty_request():
-    response = requests.post(BASE_URL, json={})
+    response = client.post("/predict", json={})
     assert response.status_code == 422
     assert "detail" in response.json()
 
-#Тест на граничные значения
+# Тест на граничные значения
 def test_api_edge_case_zero_mileage():
     sample = valid_sample.copy()
     sample["Mileage"] = 0  # 0 пробега
-    response = requests.post(BASE_URL, json=sample)
+    response = client.post("/predict", json=sample)
     assert response.status_code == 200
     json_data = response.json()
     assert "predicted_price" in json_data
 
-#Тест на корректность структуры
+# Тест на корректность структуры
 def test_response_structure():
-    response = requests.post(BASE_URL, json=valid_sample)
+    response = client.post("/predict", json=valid_sample)
     json_data = response.json()
     assert isinstance(json_data, dict)
     assert "predicted_price" in json_data
